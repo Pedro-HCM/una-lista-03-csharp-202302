3f1f153a57be653a82905dfd9fb8675b2ef46a3a
Pedro Henrique <92341351+Pedro-HCM@users.noreply.github.com>
2023-09-19 15:52:59 -0300

    3.java

---
+++ 3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Equacao{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o coeficiente 'a': ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Digite o coeficiente 'b': ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Digite o coeficiente 'c': ");
+        double c = scanner.nextDouble();
+
+        if (a == 0 && b == 0 && c != 0) {
+            System.out.println("Coeficientes informados incorretamente.");
+        } else if (a == 0 && b != 0) {
+            System.out.println("Essa é uma equação de primeiro grau.");
+            double raiz = -c / b;
+            System.out.println("Raiz real da equação: " + raiz);
+        } else {
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("Esta equação não possui raízes reais.");
+            } else if (delta == 0) {
+                System.out.println("Esta equação possui duas raízes reais iguais.");
+                double raiz = -b / (2 * a);
+                System.out.println("Valor das raízes: " + raiz);
+            } else {
+                System.out.println("Esta equação possui duas raízes reais diferentes.");
+                double raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Valor da primeira raiz: " + raiz1);
+                System.out.println("Valor da segunda raiz: " + raiz2);
+            }
+        }
+
+        scanner.close();
+    }
+}
