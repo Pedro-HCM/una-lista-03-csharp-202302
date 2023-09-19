ef581e86c7659ac75a584f216a191bc7e025e583
Pedro Henrique <92341351+Pedro-HCM@users.noreply.github.com>
2023-09-19 20:16:49 -0300

    2.java

--- 2.java
+++ 2.java
@@ -9,10 +9,12 @@ public class Main {
 
         System.out.print("Digite o valor pago: ");
         double vPago = scanner.nextDouble();
+        double trc = vPago - vCompra;
 
         if (vPago < vCompra) {
             System.out.println("Quantia paga é insuficiente para realizar a compra.");
         } else {
+            
             double troco = vPago - vCompra;
 
             int nota50 = (int) (troco / 50);
@@ -32,7 +34,7 @@ public class Main {
 
             int moeda1 = (int) (troco);
 
-            System.out.println("Troco: R$" + vPago);
+        System.out.println("Troco: R$" + trc);
             System.out.println("Notas de R$ 50,00: " + nota50);
             System.out.println("Notas de R$ 20,00: " + nota20);
             System.out.println("Notas de R$ 10,00: " + nota10);
@@ -43,4 +45,3 @@ public class Main {
 
         scanner.close();
     }
-}
